170e98b430cbfacdd2c122252bb175a4c754e16b
GarudaProjects <80512930+GarudaProjects@users.noreply.github.com>
2022-01-25 00:38:21 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,14 @@ function App() {
       
       <div className="social-buttons">
         <GitHubButton
-          href="https://github.com/SniperProject/sniper-swap"
+          href="https://github.com/GarudaProjects/SolSwap/"
           data-color-scheme="no-preference: light; light: light; dark: light;"
           data-icon="octicon-star"
           data-size="large"
           data-show-count={true}
-          aria-label="Star SniperProject/sniper-swap on GitHub"
+          aria-label="Star SolSwap on GitHub"
         >
           Star
-        </GitHubButton>
-        <GitHubButton
-          href="https://github.com/SniperProject/sniper-swap/fork"
-          data-color-scheme="no-preference: light; light: light; dark: light;"
-          data-size="large"
-          aria-label="Fork SniperProject/sniper-swap on GitHub"
-        >
-          Fork
         </GitHubButton>
       </div>
     </div>
