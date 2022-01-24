1941a19781f812b8843573ac8517f34304441195
Raden Salman <isall.mannn@gmail.com>
2022-01-25 01:13:04 +0700

    Updates

---
+++ precache-manifest.2e63d8eeb66eafdaf931e12b1c13f699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072cfc7b87f1eeb1f0ae1a940a88b4c9",
+    "url": "/SolSwap/index.html"
+  },
+  {
+    "revision": "bc662f885d2655eebdf1",
+    "url": "/SolSwap/static/css/2.3870cfe2.chunk.css"
+  },
+  {
+    "revision": "a3864a75da6187b7d286",
+    "url": "/SolSwap/static/css/main.703ab3cc.chunk.css"
+  },
+  {
+    "revision": "bc662f885d2655eebdf1",
+    "url": "/SolSwap/static/js/2.848f5797.chunk.js"
+  },
+  {
+    "revision": "1a29f8fb7f6a758d9dce8a0780f3b302",
+    "url": "/SolSwap/static/js/2.848f5797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3864a75da6187b7d286",
+    "url": "/SolSwap/static/js/main.e0304129.chunk.js"
+  },
+  {
+    "revision": "844b3a2971251548ca7b",
+    "url": "/SolSwap/static/js/runtime-main.8f68e60b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.552f09c7eccbc4f742e21863af8f1f79.js"
+  "/SolSwap/precache-manifest.2e63d8eeb66eafdaf931e12b1c13f699.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SolSwap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
